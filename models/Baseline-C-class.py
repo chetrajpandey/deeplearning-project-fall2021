cf4345781dfb6b0b3340a3309de43fa1cd2bfebf
Chetraj Pandey <chetrajpandey@gmail.com>
2021-10-29 02:04:05 -0400

    baseline added

---
+++ models/Baseline-C-class.py
@@ -0,0 +1,394 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import os
+import numpy as np
+import torch
+from torch.autograd import Variable
+from torchvision.models.vgg import model_urls
+from models.models import Custom_AlexNet, Custom_VGG16, Custom_ResNet34, Baseline_CNN
+
+# All neural network modules, nn.Linear, nn.Conv2d, BatchNorm, Loss functions
+import torch.nn as nn 
+import torch.nn.functional as F
+
+# For all Optimization algorithms, SGD, Adam, etc.
+import torch.optim as optim
+
+# Loading and Performing transformations on dataset
+import torchvision
+import torchvision.transforms as transforms 
+from torchvision.transforms import ToTensor
+from torch.utils.data import Dataset, DataLoader, ConcatDataset
+from torch.utils.data.sampler import Sampler, WeightedRandomSampler
+
+#Labels in CSV and Inputs in Fits in a folder
+import pandas as pd
+import sunpy.map
+from PIL import Image
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+#For Confusion Matrix
+from sklearn.metrics import confusion_matrix
+
+#Warnings
+# import warnings
+# warnings.simplefilter("ignore", Warning)
+
+from sklearn.model_selection import train_test_split
+# model = Baseline_CNN().to(device)
+
+
+# In[2]:
+
+
+class MyJP2Dataset(Dataset):
+    def __init__(self, csv_file, root_dir, transform=None):
+        self.annotations = pd.read_csv(csv_file)
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __getitem__(self, index):
+        img_path = os.path.join(self.root_dir, self.annotations.iloc[index, 0])
+        hmi = Image.open(img_path)
+
+        if self.transform:
+            image = self.transform(hmi)
+            
+        y_label = torch.tensor(int(self.annotations.iloc[index, 1]))
+        
+        return (image, y_label)
+
+    def __len__(self):
+        return len(self.annotations)
+    
+class RotateAugmentation(Dataset):
+    def __init__(self, csv_file, root_dir, transform=None):
+        iter_csv = pd.read_csv(csv_file, iterator=True)
+        df = pd.concat([chunk[chunk['goes_class'] == 1] for chunk in iter_csv])
+        self.annotations = df
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __getitem__(self, index):
+        img_path = os.path.join(self.root_dir, self.annotations.iloc[index, 0])
+        hmi = Image.open(img_path)
+        y_label = torch.tensor(int(self.annotations.iloc[index, 1]))
+        #print(y_label)
+
+        if self.transform:
+            image = self.transform(hmi)
+            
+        return (image, y_label)
+
+    def __len__(self):
+        return len(self.annotations)
+    
+class FlipAugmentation(Dataset):
+    def __init__(self, csv_file, root_dir, transform=None):
+        iter_csv = pd.read_csv(csv_file, iterator=True)
+        df = pd.concat([chunk[chunk['goes_class'] == 1] for chunk in iter_csv])
+        self.annotations = df
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __getitem__(self, index):
+        img_path = os.path.join(self.root_dir, self.annotations.iloc[index, 0])
+        hmi = Image.open(img_path)
+        y_label = torch.tensor(int(self.annotations.iloc[index, 1]))
+        #print(y_label)
+
+        if self.transform:
+            image = self.transform(hmi)
+            
+        return (image, y_label)
+
+    def __len__(self):
+        return len(self.annotations)
+
+
+# In[3]:
+
+
+# CUDA for PyTorch
+use_cuda = torch.cuda.is_available()
+device = torch.device('cuda:1' if use_cuda else 'cpu')
+# device = torch.device('cpu')
+torch.backends.cudnn.benchmark = True
+print(device)
+
+
+# In[4]:
+
+
+# Load Data
+datapath = '/data/hmi_BI_daily/aiapng/'
+labelpath = '/data/hmi_BI_daily/aia_data_labels/C_Bin_Fold1_train.csv'
+val_labelpath = '/data/hmi_BI_daily/aia_data_labels/C_Bin_Fold1_val.csv'
+
+transformations = transforms.Compose([
+    transforms.Resize(512),
+    transforms.ToTensor()
+])
+
+
+rotation = transforms.Compose([
+    transforms.RandomRotation(degrees=(-5,5)),
+    transforms.Resize(512),
+    transforms.ToTensor()
+])
+
+
+flip = transforms.Compose([
+    transforms.RandomChoice([
+    transforms.RandomHorizontalFlip(p=1.0),
+    transforms.RandomVerticalFlip(p=1.0)]),
+    transforms.Resize(512),
+    transforms.ToTensor()
+])
+
+#Train Time-Based Split -- Data from 2010-2014 in Train Set
+original_train = MyJP2Dataset(csv_file = labelpath, 
+                             root_dir = datapath,
+                             transform = transformations)
+rotation_set = RotateAugmentation(csv_file = labelpath, 
+                             root_dir = datapath,
+                             transform = rotation)
+
+flip_set = FlipAugmentation(csv_file = labelpath, 
+                             root_dir = datapath,
+                             transform = flip)
+
+train_set = ConcatDataset([flip_set, original_train, rotation_set])
+print(2*len(rotation_set), len(original_train), len(train_set))
+#Data From 2015-2018 are randomly splitted into 40% and 60% into validation and test set resp.
+val_set = MyJP2Dataset(csv_file = val_labelpath, 
+                             root_dir = datapath,
+                             transform = transformations)
+
+
+# In[5]:
+
+
+def imbalance_weights(dataset):
+    target = []
+    with torch.no_grad():
+        for i in range(len(dataset)):
+            target.append(np.asarray(dataset[i][1]))
+            if i%500 == 0:
+                print(f'{(i*100)/len(dataset):.2f}% Complete')
+        targets = np.asarray(target)
+        csc = np.array([len(np.where(targets == t)[0]) for t in np.unique(targets)])
+        print(csc)
+        majority_weight = 1/csc[0]
+        minority_weight = 1/csc[1]
+        sample_weights = np.array([majority_weight, minority_weight])
+        samples_weight = sample_weights[targets]
+        samples_weight = torch.from_numpy(samples_weight)
+        return samples_weight
+
+train_samples_weight = imbalance_weights(train_set)
+#test_samples_weight = imbalance_weights(test_set)
+#val_samples_weight = imbalance_weights(val_set)
+
+# train_samples_weight = torch.load('train_samples_weight.pt')
+
+
+# In[6]:
+
+
+batch_size = 24
+sampler1   = WeightedRandomSampler(weights=train_samples_weight, num_samples= len(train_samples_weight), replacement=True)
+#sampler2  = WeightedRandomSampler(weights=test_samples_weight, num_samples= len(test_samples_weight), replacement=True)
+#sampler3  = WeightedRandomSampler(weights=val_samples_weight, num_samples= int(200), replacement=False)
+
+train_loader = DataLoader(dataset=train_set, batch_size=batch_size, sampler=sampler1, num_workers=0, shuffle = False)
+
+val_loader = DataLoader(dataset=val_set, batch_size=batch_size, num_workers=0, shuffle=False)
+
+
+# In[7]:
+
+
+# Hyperparameters
+in_channel = 1
+#num_classes = 2
+learning_rate = 1e-2
+#batch_size = 50
+num_epochs = 80
+
+# Initialize network
+model=None
+# print(model)
+# device = torch.device('cpu')
+model = Baseline_CNN().to(device)
+
+# Loss and optimizer
+criterion = nn.CrossEntropyLoss()
+# criterion = nn.NLLLoss()
+# optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, weight_decay=1e-6)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=4)
+
+
+# In[8]:
+
+
+def accuracy_score(prediction, target):
+    TN, FP, FN, TP = confusion_matrix(target, prediction).ravel()
+    print("TP: ", TP, "FP: ", FP, "TN: ", TN, "FN: ", FN)
+    #TSS Computation also known as "recall"
+    tp_rate = TP / float(TP + FN) if TP > 0 else 0  
+    fp_rate = FP / float(FP + TN) if FP > 0 else 0
+    TSS = tp_rate - fp_rate
+    
+    #HSS2 Computation
+    N = TN + FP
+    P = TP + FN
+    HSS = (2 * (TP * TN - FN * FP)) / float((P * (FN + TN) + (TP + FP) * N))
+    
+    #F0.5 Score Computation
+    fscore = (1 + (0.5**2)*TP)/ ((1 + (0.5**2)*TP) + (0.5**2)*FN + FP)
+
+    return TSS, HSS, fscore
+
+
+# In[9]:
+
+
+def sklearn_Compatible_preds_and_targets(model_prediction_list, model_target_list):
+    y_pred_list = []
+    preds = []
+    target_list = []
+    tgts = []
+    y_pred_list = [a.squeeze().tolist() for a in model_prediction_list]
+    preds = [item for sublist in y_pred_list for item in sublist]
+    target_list = [a.squeeze().tolist() for a in model_target_list]
+    tgts = [item for sublist in target_list for item in sublist]
+    return accuracy_score(preds, tgts)
+
+
+# In[ ]:
+
+
+# Training Network
+print("Training in Progress..")
+train_loss_values = []
+val_loss_values = []
+train_tss_values = []
+val_tss_values = []
+train_hss_values = []
+val_hss_values = []
+train_fscore_values = []
+val_fscore_values = []
+for epoch in range(num_epochs):
+    
+    # setting the model to train mode
+    model.train()
+    train_loss = 0
+    train_tss = 0.
+    train_hss = 0.
+    train_prediction_list = []
+    train_target_list = []
+    for batch_idx, (data, targets) in enumerate(train_loader):
+        # Get data to cuda if possible
+        data = data.to(device=device)
+        targets = targets.to(device=device)
+        train_target_list.append(targets)
+        
+        # forward prop
+        scores = model(data)
+        loss = criterion(scores, targets)
+        _, predictions = torch.max(scores,1)
+        train_prediction_list.append(predictions)
+        
+        # backward prop
+        optimizer.zero_grad()
+        loss.backward()
+        
+        # Adam step
+        optimizer.step()
+        
+        # accumulate the training loss
+        #print(loss.item())
+        train_loss += loss.item()
+        #train_acc+= acc.item()
+        
+        
+    # Validation: setting the model to eval mode
+    model.eval()
+    val_loss = 0.
+    val_tss = 0.
+    val_hss = 0.
+    val_prediction_list = []
+    val_target_list = []
+    # Turning off gradients for validation
+    with torch.no_grad():
+        for d, t in val_loader:
+            # Get data to cuda if possible
+            d = d.to(device=device)
+            t = t.to(device=device)
+            val_target_list.append(t)
+            
+            # forward pass
+            s = model(d)
+            #print("scores", s)
+                                   
+            # validation batch loss and accuracy
+            l = criterion(s, t)
+            _, p = torch.max(s,1)
+            #print("------------------------------------------------")
+            #print(torch.max(s,1))
+            #print('final', p)
+            val_prediction_list.append(p)
+            
+            # accumulating the val_loss and accuracy
+            val_loss += l.item()
+            #val_acc += acc.item()
+            del d,t,s,l,p
+    scheduler.step(val_loss)
+            
+    #Epoch Results
+    train_loss /= len(train_loader)
+    train_loss_values.append(train_loss)
+    val_loss /= len(val_loader)
+    val_loss_values.append(val_loss)
+    train_tss, train_hss, train_fscore = sklearn_Compatible_preds_and_targets(train_prediction_list, train_target_list)
+    train_tss_values.append(train_tss)
+    train_hss_values.append(train_hss)
+    train_fscore_values.append(train_fscore)
+    val_tss, val_hss, val_fscore = sklearn_Compatible_preds_and_targets(val_prediction_list, val_target_list)
+    val_tss_values.append(val_tss)
+    val_hss_values.append(val_hss)
+    val_fscore_values.append(val_fscore)
+    print(f'Epoch: {epoch+1}/{num_epochs}')
+    print(f'Training--> loss: {train_loss:.4f}, TSS: {train_tss:.4f}, HSS2: {train_hss:.4f}, F0.5 Score: {train_fscore:.4f} | Val--> loss: {val_loss:.4f}, TSS: {val_tss:.4f} , HSS2: {val_hss:.4f}, F0.5 Score: {val_fscore:.4f} ')
+
+
+# In[ ]:
+
+
+print(model)
+
+
+# In[ ]:
+
+
+train_samples_weight
+
+
+# In[ ]:
+
+
+# torch.save(train_samples_weight, 'train_samples_weight.pt') 
+
+
+# In[ ]:
+
+
+
+
